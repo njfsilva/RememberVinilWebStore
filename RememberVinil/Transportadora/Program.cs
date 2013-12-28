456b1fe083bfd170ee1c1600a95e3b8fd61af86d
Dyox <carlosrok2003@gmail.com>
2013-12-28 12:42:15 +0000

    transportadora, falta por em webservice
    o prof fez isto no programa do chat, mas n sei como ele fez

--- RememberVinil/Transportadora/Program.cs
+++ RememberVinil/Transportadora/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.ServiceModel;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Transportadora
 {
     class Program
     {
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        //{
+        //    Console.ReadLine();
+        //}
+    }
+
+    public class Coordinates
+    {
+        public string longitude;
+        public string latitude;
+    }
+
+    public class ObjectTransportRequest
+    {
+        public Coordinates origem;
+        public Coordinates destino;
+        public string wsUrl; /* o url da callback */
+	    public string refRequestOriginDestination; /* uma referência para associar as duas mensagens */
+    }
+
+    public class ObjectTransportJobResponse
+    {
+        public int ID;
+        public string refRequestOriginDestination; /* uma referência para associar as duas mensagens */
+    }
+
+    [ServiceContract]
+    public interface Transportadora
+    {
+        [OperationContract]
+        ObjectTransportJobResponse TransportJob(ObjectTransportRequest request);
+
+    }
+
+    public class TransportadoraImpl : Transportadora
+    {
+        public ObjectTransportJobResponse TransportJob(ObjectTransportRequest request)
         {
-            Console.ReadLine();
+            ObjectTransportJobResponse r = new ObjectTransportJobResponse();
+            Random rnd = new Random();
+            r.ID = rnd.Next(1024);
+            r.refRequestOriginDestination = "";
+            return r;
         }
     }
 }
