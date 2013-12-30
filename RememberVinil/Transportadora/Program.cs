8a4d99141b146da7bcd3467232aed8e0cd2b455b
Dyox <carlosrok2003@gmail.com>
2013-12-30 16:28:42 +0000

    removido o nelson já fez

--- RememberVinil/Transportadora/Program.cs
+++ RememberVinil/Transportadora/Program.cs
@@ -16,44 +16,4 @@ namespace Transportadora
             Console.ReadLine();
         }
     }
-
-    public class Coordinates
-    {
-        public string longitude;
-        public string latitude;
-    }
-
-    public class ObjectTransportRequest
-    {
-        public Coordinates origem;
-        public Coordinates destino;
-        public string wsUrl; /* o url da callback */
-	    public string refRequestOriginDestination; /* uma referência para associar as duas mensagens */
-    }
-
-    public class ObjectTransportJobResponse
-    {
-        public int ID;
-        public string refRequestOriginDestination; /* uma referência para associar as duas mensagens */
-    }
-
-    [ServiceContract]
-    public interface Transportadora
-    {
-        [OperationContract]
-        ObjectTransportJobResponse TransportJob(ObjectTransportRequest request);
-
-    }
-
-    public class TransportadoraImpl : Transportadora
-    {
-        public ObjectTransportJobResponse TransportJob(ObjectTransportRequest request)
-        {
-            ObjectTransportJobResponse r = new ObjectTransportJobResponse();
-            Random rnd = new Random();
-            r.ID = rnd.Next(1024);
-            r.refRequestOriginDestination = "";
-            return r;
-        }
-    }
 }
