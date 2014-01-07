1d83be93a0688535d92549871e06fe0902c79765
Dyox <carlosrok2003@gmail.com>
2014-01-07 08:34:16 +0000

    services updated

--- RememberVinil/FabricanteA/Service References/BOCallBack/Reference.cs
+++ RememberVinil/FabricanteA/Service References/BOCallBack/Reference.cs
@@ -9,6 +9,10 @@
 //------------------------------------------------------------------------------
 
 namespace FabricanteA.BOCallBack {
+    using System.Runtime.Serialization;
+    using System;
+    
+    
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
     [System.Runtime.Serialization.DataContractAttribute(Name="ObjectMakeCDResponse", Namespace="http://schemas.datacontract.org/2004/07/BackOffice")]
@@ -150,6 +154,147 @@ namespace FabricanteA.BOCallBack {
             }
             set {
                 if ((this.userIDField.Equals(value) != true)) {
+                    this.userIDField = value;
+                    this.RaisePropertyChanged("userID");
+                }
+            }
+        }
+        
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        
+        protected void RaisePropertyChanged(string propertyName) {
+            System.ComponentModel.PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if ((propertyChanged != null)) {
+                propertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+    
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
+    [System.Runtime.Serialization.DataContractAttribute(Name="TransportJobResponse", Namespace="http://schemas.datacontract.org/2004/07/BackOffice")]
+    [System.SerializableAttribute()]
+    public partial class TransportJobResponse : object, System.Runtime.Serialization.IExtensibleDataObject, System.ComponentModel.INotifyPropertyChanged {
+        
+        [System.NonSerializedAttribute()]
+        private System.Runtime.Serialization.ExtensionDataObject extensionDataField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private string DeliveryAdressField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private string DistanceField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private string StatusField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private bool SucessField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private string encomendaIDField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private string fabricaField;
+        
+        [System.Runtime.Serialization.OptionalFieldAttribute()]
+        private string userIDField;
+        
+        [global::System.ComponentModel.BrowsableAttribute(false)]
+        public System.Runtime.Serialization.ExtensionDataObject ExtensionData {
+            get {
+                return this.extensionDataField;
+            }
+            set {
+                this.extensionDataField = value;
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public string DeliveryAdress {
+            get {
+                return this.DeliveryAdressField;
+            }
+            set {
+                if ((object.ReferenceEquals(this.DeliveryAdressField, value) != true)) {
+                    this.DeliveryAdressField = value;
+                    this.RaisePropertyChanged("DeliveryAdress");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public string Distance {
+            get {
+                return this.DistanceField;
+            }
+            set {
+                if ((object.ReferenceEquals(this.DistanceField, value) != true)) {
+                    this.DistanceField = value;
+                    this.RaisePropertyChanged("Distance");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public string Status {
+            get {
+                return this.StatusField;
+            }
+            set {
+                if ((object.ReferenceEquals(this.StatusField, value) != true)) {
+                    this.StatusField = value;
+                    this.RaisePropertyChanged("Status");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public bool Sucess {
+            get {
+                return this.SucessField;
+            }
+            set {
+                if ((this.SucessField.Equals(value) != true)) {
+                    this.SucessField = value;
+                    this.RaisePropertyChanged("Sucess");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public string encomendaID {
+            get {
+                return this.encomendaIDField;
+            }
+            set {
+                if ((object.ReferenceEquals(this.encomendaIDField, value) != true)) {
+                    this.encomendaIDField = value;
+                    this.RaisePropertyChanged("encomendaID");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public string fabrica {
+            get {
+                return this.fabricaField;
+            }
+            set {
+                if ((object.ReferenceEquals(this.fabricaField, value) != true)) {
+                    this.fabricaField = value;
+                    this.RaisePropertyChanged("fabrica");
+                }
+            }
+        }
+        
+        [System.Runtime.Serialization.DataMemberAttribute()]
+        public string userID {
+            get {
+                return this.userIDField;
+            }
+            set {
+                if ((object.ReferenceEquals(this.userIDField, value) != true)) {
                     this.userIDField = value;
                     this.RaisePropertyChanged("userID");
                 }
@@ -391,6 +536,9 @@ namespace FabricanteA.BOCallBack {
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IBackOfficeCallBackService/GetStatus", ReplyAction="http://tempuri.org/IBackOfficeCallBackService/GetStatusResponse")]
         string GetStatus(FabricanteA.BOCallBack.ObjectMakeCDResponse response);
         
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IBackOfficeCallBackService/UpdateOrderTransportStatus", ReplyAction="http://tempuri.org/IBackOfficeCallBackService/UpdateOrderTransportStatusResponse")]
+        string UpdateOrderTransportStatus(FabricanteA.BOCallBack.TransportJobResponse response);
+        
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IBackOfficeCallBackService/GetTransporterPrice", ReplyAction="http://tempuri.org/IBackOfficeCallBackService/GetTransporterPriceResponse")]
         string GetTransporterPrice(FabricanteA.BOCallBack.TransportJobPriceResponse response);
         
@@ -427,6 +575,10 @@ namespace FabricanteA.BOCallBack {
         
         public string GetStatus(FabricanteA.BOCallBack.ObjectMakeCDResponse response) {
             return base.Channel.GetStatus(response);
+        }
+        
+        public string UpdateOrderTransportStatus(FabricanteA.BOCallBack.TransportJobResponse response) {
+            return base.Channel.UpdateOrderTransportStatus(response);
         }
         
         public string GetTransporterPrice(FabricanteA.BOCallBack.TransportJobPriceResponse response) {
